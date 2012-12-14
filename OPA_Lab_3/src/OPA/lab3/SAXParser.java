2720ad3a537576eac176a5cd6808e0b4ad75f108
Miloszek <miloszj7@gmail.com>
2012-12-14 19:26:23 +0100

    zwei

--- OPA_Lab_3/src/OPA/lab3/SAXParser.java
+++ OPA_Lab_3/src/OPA/lab3/SAXParser.java
@@ -7,7 +7,7 @@ public class SAXParser {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("out"); 
+		System.out.println("out, jap"); 
 	}
 
 }
