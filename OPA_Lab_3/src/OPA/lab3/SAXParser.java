cb276927eff8acc5010e1db372c777808f5c94cb
Miloszek <miloszj7@gmail.com>
2012-12-14 19:58:20 +0100

    started!

--- OPA_Lab_3/src/OPA/lab3/SAXParser.java
+++ OPA_Lab_3/src/OPA/lab3/SAXParser.java
@@ -1,13 +1,38 @@
 package OPA.lab3;
 
-public class SAXParser {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class SAXParser {
+	private static final String FILEPATH = ""; 
+	class readXMLFIle{
+		
+		
+		
+	}
+	
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("out, jap"); 
+				
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(new File(FILEPATH)));
+			String s, s2 = new String();
+		    while((s = br.readLine())!= null)
+		      s2 += s + "\n";
+		    br.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO: handle exception
+			System.err.println(e.toString());
+		}
 	}
 
 }
